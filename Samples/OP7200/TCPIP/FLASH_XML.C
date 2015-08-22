94317d89abd448eab8345274a5527a88247df062
Digi International <git@digi.com>
2015-08-21 17:42:32 -0700

    license: update copyright notices in all source code files

--- Samples/OP7200/TCPIP/FLASH_XML.C
+++ Samples/OP7200/TCPIP/FLASH_XML.C
@@ -1,6 +1,20 @@
+/*
+   Copyright (c) 2015, Digi International Inc.
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
 /***************************************************************************
         Samples\OP7200\TCPIP\FLASH_XML.c
-        Z-World, 2002
 
         The basic idea for this program is to allow a html web page to create
          a separate active socket on a server which runs on the OP7200.
